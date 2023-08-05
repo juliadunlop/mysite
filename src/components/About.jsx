5d18143904a7ffabf2bf23f6aa27e8ebd5904bcd
juliadunlop <jlddunlop@gmail.com>
2023-08-05 08:01:08 -0700

    updated about, customabouttab and styles files

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,23 +1,33 @@
 import React from "react";
-import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
-import 'react-tabs/style/react-tabs.css';
+import CustomAboutTab from "./CustomAboutTab";
 
 function About() {
     return <div class = "box">
         <div class ="rowname">
             <div class = "column">
-                <h1>J</h1>
-                <h1>U</h1>
-                <h1>L</h1>
-                <h1>I</h1>
-                <h1>A</h1>
+                <div class = "name">
+                    <h1>J</h1>
+                    <h1>U</h1>
+                    <h1>L</h1>
+                    <h1>I</h1>
+                    <h1>A</h1>
+                </div>
+                
                 <br></br>
-                <h1>D</h1>
-                <h1>U</h1>
-                <h1>N</h1>
-                <h1>L</h1>
-                <h1>O</h1>
-                <h1>P</h1>
+                <div class="break">
+                <div>
+                    <h1>D</h1>
+                    <h1>U</h1>
+                    <h1>N</h1>
+                </div>
+
+                <div>
+                    <h1>L</h1>
+                    <h1>O</h1>
+                    <h1>P</h1>
+                </div>
+                </div>
+                
             </div>
             <div class ="column">
                 <p>*insert graphic</p>
@@ -26,6 +36,7 @@ function About() {
         </div>
         
 
+        <br></br>
         <br></br>
 
         <div class = "row">
@@ -36,54 +47,7 @@ function About() {
         <br></br>
         <br></br>
         <br></br>
-
-        <div class = "row tabrow">
-            <Tabs>
-                <TabList>
-                    <Tab><h2 class="tab">Software Engineering *change colour when active</h2></Tab>
-                    <Tab><h2 class="tab">Civil Engineering</h2></Tab>
-                    <Tab><h2 class="tab">Ballet</h2></Tab>
-                </TabList>
-                <div class = "tabPanel">
-                <TabPanel>
-                        <h2>Education</h2>
-                        <p><a href="https://www.google.com">Bachelors of Science in How to Google</a> - Google - 2023</p>
-                        <br></br>
-
-                        <h2>Projects</h2>
-                        <p><a href="https://www.ubc.ca">Sequence</a> - The teachers aid</p>
-                        <p><a href="https://www.ubc.ca">Rigging</a> - Optimized pick points for Mass Timber construction</p>
-                        <br></br>
-                        <h4 class="resume"><a href="/softwareresume">Full CV...</a></h4>
-                </TabPanel>
-                <TabPanel>
-                        <h2>Education</h2>
-                        <p><a href="https://www.ubc.ca">Bachelors of Applied Science in Civil Engineering</a> - University of British Columbia - 2019 </p>
-                        <br></br>
-                        <h2>Experience</h2>
-                        <p><a href="https://www.bhbengineers.com">BHB Engineers</a> - Project Engineer</p>
-                        <p><a href="https://www.smartlam.com/">Smartlam</a> - Project Engineer</p>
-                        <p><a href="https://www.eclipse-engineering.com/">Eclipse Engineering</a> - CLT Team Lead, Project Manager, Project Engineer</p>
-                        <br></br>
-                        <h4 class="resume"><a href="/civilresume">Full CV...</a></h4>
-                </TabPanel>
-                <TabPanel>
-                        <h2>Training</h2>
-                        <p><a href="https://www.balletidaho.org">Ballet Idaho</a> - Heather Hawk</p>
-                        <p><a href="https://www.balletchicago.org/">Ballet Chicago</a> - Summer Intensive 2011</p>
-                        <p><a href="https://www.pnb.org">Pacific Northwest Ballet</a> - Summer Intensive 2012 and 2013</p>
-                        <br></br>
-                        <h2>Professional Experience</h2>
-                        <p><a href="https://www.balletidaho.org">Ballet Idaho</a> - Corps de Ballet 2011-2013</p>
-                        <br></br>
-                        <h2>Teaching</h2>
-                        <p><a href="https://www.echodance.com/">Echo School of Dance</a> - Upper Level Technique and Repertoire</p>
-                        <br></br>
-                        <h4 class="resume"><a href="/balletresume">Full CV...</a></h4>   
-                </TabPanel>   
-                </div> 
-            </Tabs>
-        </div>
+        <CustomAboutTab/>
         </div>
 
         
